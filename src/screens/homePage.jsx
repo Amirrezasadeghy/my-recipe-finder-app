933b52444d26c8f0197ec2780603d981c12ce10c
Amirrezasadeghy <amirrzsadeghi@gmail.com>
2024-06-16 19:54:04 -0700

    App Updated

---
+++ src/screens/homePage.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { useState } from 'react';
+
+import SearchBar from '../components/searchbar';
+import RecipeCard from '../components/recipeCard';
+import Pagination from '../components/Pagination';
+
+import { fetchRecipes } from '../utils/api';
+
+
+const HomePage = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const handleSearch = async () => {
+    setLoading(true);
+    const data = await
+
+    fetchRecipes(searchQuery, currentPage);
+    setRecipes(data.hits.map(hit => hit.recipe));
+
+    setTotalPages(Math.ceil(data.count / 9));
+
+    setLoading(false);
+  };
+
+  const handlePageChange = async (page) => {
+    setCurrentPage(page);
+    handleSearch();
+  };
+
+  return (
+    <div className='container mx-auto p-4'>
+      <SearchBar 
+      searchQuery={searchQuery}
+      setSearchQuery={setSearchQuery}
+      handleSearch={handleSearch}
+      />
+
+      {loading && <p>Loading...</p>}
+
+      <div className='grid grid-cols-3 gap-4'>
+
+        {recipes.map((recipe, index) => (
+
+          <RecipeCard 
+          key={index}
+          recipe={recipe}
+          />
+
+        ))}
+
+      </div>
+
+      <Pagination 
+      currentPage={currentPage}
+      totalPages={totalPages}
+      onPageChange={handlePageChange}
+      />
+    </div>
+  );
+
+};
+
+export default HomePage;
